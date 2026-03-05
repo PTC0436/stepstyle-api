d57659b99b2e415d41b3496e31026c0773c287e9
PTC0436 <phatthanhchau123@gmail.com>
2026-03-05 22:37:53 +0700

    Fix shoeController

--- controllers/shoeController.js
+++ controllers/shoeController.js
@@ -77,6 +77,7 @@ export const getShoes = async (req, res) => {
       shoesLength: shoes.length,
       skip: skip,
       limit: limitNum,
+      filter: filter,
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
