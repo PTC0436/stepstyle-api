77b8032210e82f83014527fe40cac9b6983474e4
PTC0436 <phatthanhchau123@gmail.com>
2026-03-10 18:20:20 +0700

    Fix authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,12 +22,10 @@ export const register = async (req, res) => {
       });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password,
       address,
     });
 
